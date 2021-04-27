b5b7e5a10f8f84db1b6392696603f7cbaec56a3f
mario5698 <aranicbiagm@gmail.com>
2021-04-27 20:41:59 +0200

    hyperSpace start project and load routes maps and get information

--- HyperSpaceSystem/HyperSpaceSystem/Form1.Designer.cs
+++ HyperSpaceSystem/HyperSpaceSystem/Form1.Designer.cs
@@ -29,13 +29,251 @@ namespace HyperSpaceSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lsb_routes = new System.Windows.Forms.ListBox();
+            this.lbl_filiation = new System.Windows.Forms.Label();
+            this.lbl_filitationText = new System.Windows.Forms.Label();
+            this.lbl_longText = new System.Windows.Forms.Label();
+            this.lbl_long = new System.Windows.Forms.Label();
+            this.lbl_routesText = new System.Windows.Forms.Label();
+            this.lbl_nativesText = new System.Windows.Forms.Label();
+            this.lbl_sector = new System.Windows.Forms.Label();
+            this.lbl_sectorText = new System.Windows.Forms.Label();
+            this.lbl_latText = new System.Windows.Forms.Label();
+            this.lbl_lat = new System.Windows.Forms.Label();
+            this.lbl_natives = new System.Windows.Forms.Label();
+            this.lbl_planet = new System.Windows.Forms.Label();
+            this.lbl_planetText = new System.Windows.Forms.Label();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.cmb_planet = new System.Windows.Forms.ComboBox();
+            this.cmb_category = new System.Windows.Forms.ComboBox();
+            this.cmb_position = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(587, 174);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 152);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 35;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lsb_routes
+            // 
+            this.lsb_routes.FormattingEnabled = true;
+            this.lsb_routes.Location = new System.Drawing.Point(373, 325);
+            this.lsb_routes.Name = "lsb_routes";
+            this.lsb_routes.Size = new System.Drawing.Size(180, 82);
+            this.lsb_routes.TabIndex = 34;
+            // 
+            // lbl_filiation
+            // 
+            this.lbl_filiation.AutoSize = true;
+            this.lbl_filiation.Location = new System.Drawing.Point(512, 144);
+            this.lbl_filiation.Name = "lbl_filiation";
+            this.lbl_filiation.Size = new System.Drawing.Size(55, 13);
+            this.lbl_filiation.TabIndex = 33;
+            this.lbl_filiation.Text = "lbl_filiation";
+            // 
+            // lbl_filitationText
+            // 
+            this.lbl_filitationText.AutoSize = true;
+            this.lbl_filitationText.Location = new System.Drawing.Point(512, 124);
+            this.lbl_filitationText.Name = "lbl_filitationText";
+            this.lbl_filitationText.Size = new System.Drawing.Size(42, 13);
+            this.lbl_filitationText.TabIndex = 32;
+            this.lbl_filitationText.Text = "filitation";
+            // 
+            // lbl_longText
+            // 
+            this.lbl_longText.AutoSize = true;
+            this.lbl_longText.Location = new System.Drawing.Point(512, 174);
+            this.lbl_longText.Name = "lbl_longText";
+            this.lbl_longText.Size = new System.Drawing.Size(50, 13);
+            this.lbl_longText.TabIndex = 31;
+            this.lbl_longText.Text = "longitude";
+            // 
+            // lbl_long
+            // 
+            this.lbl_long.AutoSize = true;
+            this.lbl_long.Location = new System.Drawing.Point(512, 194);
+            this.lbl_long.Name = "lbl_long";
+            this.lbl_long.Size = new System.Drawing.Size(66, 13);
+            this.lbl_long.TabIndex = 30;
+            this.lbl_long.Text = "lbl_longitude";
+            // 
+            // lbl_routesText
+            // 
+            this.lbl_routesText.AutoSize = true;
+            this.lbl_routesText.Location = new System.Drawing.Point(370, 299);
+            this.lbl_routesText.Name = "lbl_routesText";
+            this.lbl_routesText.Size = new System.Drawing.Size(41, 13);
+            this.lbl_routesText.TabIndex = 29;
+            this.lbl_routesText.Text = "Routes";
+            // 
+            // lbl_nativesText
+            // 
+            this.lbl_nativesText.AutoSize = true;
+            this.lbl_nativesText.Location = new System.Drawing.Point(370, 224);
+            this.lbl_nativesText.Name = "lbl_nativesText";
+            this.lbl_nativesText.Size = new System.Drawing.Size(41, 13);
+            this.lbl_nativesText.TabIndex = 28;
+            this.lbl_nativesText.Text = "natives";
+            // 
+            // lbl_sector
+            // 
+            this.lbl_sector.AutoSize = true;
+            this.lbl_sector.Location = new System.Drawing.Point(370, 194);
+            this.lbl_sector.Name = "lbl_sector";
+            this.lbl_sector.Size = new System.Drawing.Size(52, 13);
+            this.lbl_sector.TabIndex = 27;
+            this.lbl_sector.Text = "lbl_sector";
+            // 
+            // lbl_sectorText
+            // 
+            this.lbl_sectorText.AutoSize = true;
+            this.lbl_sectorText.Location = new System.Drawing.Point(370, 174);
+            this.lbl_sectorText.Name = "lbl_sectorText";
+            this.lbl_sectorText.Size = new System.Drawing.Size(36, 13);
+            this.lbl_sectorText.TabIndex = 26;
+            this.lbl_sectorText.Text = "sector";
+            // 
+            // lbl_latText
+            // 
+            this.lbl_latText.AutoSize = true;
+            this.lbl_latText.Location = new System.Drawing.Point(512, 224);
+            this.lbl_latText.Name = "lbl_latText";
+            this.lbl_latText.Size = new System.Drawing.Size(41, 13);
+            this.lbl_latText.TabIndex = 25;
+            this.lbl_latText.Text = "latitude";
+            // 
+            // lbl_lat
+            // 
+            this.lbl_lat.AutoSize = true;
+            this.lbl_lat.Location = new System.Drawing.Point(512, 244);
+            this.lbl_lat.Name = "lbl_lat";
+            this.lbl_lat.Size = new System.Drawing.Size(57, 13);
+            this.lbl_lat.TabIndex = 24;
+            this.lbl_lat.Text = "lbl_latitude";
+            // 
+            // lbl_natives
+            // 
+            this.lbl_natives.AutoSize = true;
+            this.lbl_natives.Location = new System.Drawing.Point(370, 244);
+            this.lbl_natives.Name = "lbl_natives";
+            this.lbl_natives.Size = new System.Drawing.Size(57, 13);
+            this.lbl_natives.TabIndex = 23;
+            this.lbl_natives.Text = "lbl_natives";
+            // 
+            // lbl_planet
+            // 
+            this.lbl_planet.AutoSize = true;
+            this.lbl_planet.Location = new System.Drawing.Point(370, 144);
+            this.lbl_planet.Name = "lbl_planet";
+            this.lbl_planet.Size = new System.Drawing.Size(52, 13);
+            this.lbl_planet.TabIndex = 22;
+            this.lbl_planet.Text = "lbl_planet";
+            // 
+            // lbl_planetText
+            // 
+            this.lbl_planetText.AutoSize = true;
+            this.lbl_planetText.Location = new System.Drawing.Point(370, 124);
+            this.lbl_planetText.Name = "lbl_planetText";
+            this.lbl_planetText.Size = new System.Drawing.Size(36, 13);
+            this.lbl_planetText.TabIndex = 21;
+            this.lbl_planetText.Text = "planet";
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(692, 67);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 23);
+            this.btn_Search.TabIndex = 20;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // cmb_planet
+            // 
+            this.cmb_planet.FormattingEnabled = true;
+            this.cmb_planet.Location = new System.Drawing.Point(532, 69);
+            this.cmb_planet.Name = "cmb_planet";
+            this.cmb_planet.Size = new System.Drawing.Size(121, 21);
+            this.cmb_planet.TabIndex = 19;
+            // 
+            // cmb_category
+            // 
+            this.cmb_category.FormattingEnabled = true;
+            this.cmb_category.Location = new System.Drawing.Point(373, 69);
+            this.cmb_category.Name = "cmb_category";
+            this.cmb_category.Size = new System.Drawing.Size(121, 21);
+            this.cmb_category.TabIndex = 18;
+            this.cmb_category.SelectedIndexChanged += new System.EventHandler(this.cmb_category_SelectedIndexChanged);
+            // 
+            // cmb_position
+            // 
+            this.cmb_position.FormattingEnabled = true;
+            this.cmb_position.Location = new System.Drawing.Point(40, 69);
+            this.cmb_position.Name = "cmb_position";
+            this.cmb_position.Size = new System.Drawing.Size(121, 21);
+            this.cmb_position.TabIndex = 36;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmb_position);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lsb_routes);
+            this.Controls.Add(this.lbl_filiation);
+            this.Controls.Add(this.lbl_filitationText);
+            this.Controls.Add(this.lbl_longText);
+            this.Controls.Add(this.lbl_long);
+            this.Controls.Add(this.lbl_routesText);
+            this.Controls.Add(this.lbl_nativesText);
+            this.Controls.Add(this.lbl_sector);
+            this.Controls.Add(this.lbl_sectorText);
+            this.Controls.Add(this.lbl_latText);
+            this.Controls.Add(this.lbl_lat);
+            this.Controls.Add(this.lbl_natives);
+            this.Controls.Add(this.lbl_planet);
+            this.Controls.Add(this.lbl_planetText);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.cmb_planet);
+            this.Controls.Add(this.cmb_category);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lsb_routes;
+        private System.Windows.Forms.Label lbl_filiation;
+        private System.Windows.Forms.Label lbl_filitationText;
+        private System.Windows.Forms.Label lbl_longText;
+        private System.Windows.Forms.Label lbl_long;
+        private System.Windows.Forms.Label lbl_routesText;
+        private System.Windows.Forms.Label lbl_nativesText;
+        private System.Windows.Forms.Label lbl_sector;
+        private System.Windows.Forms.Label lbl_sectorText;
+        private System.Windows.Forms.Label lbl_latText;
+        private System.Windows.Forms.Label lbl_lat;
+        private System.Windows.Forms.Label lbl_natives;
+        private System.Windows.Forms.Label lbl_planet;
+        private System.Windows.Forms.Label lbl_planetText;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.ComboBox cmb_planet;
+        private System.Windows.Forms.ComboBox cmb_category;
+        private System.Windows.Forms.ComboBox cmb_position;
     }
 }
 
