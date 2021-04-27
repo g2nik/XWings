1d7fe43f5bcb48326dfd2bf05f55535c33a520ed
g2nik <nikita.chechel@sarria.salesians.cat>
2021-04-27 16:35:43 +0200

    Linq advanced

---
+++ OnBoardComputer/OnBoardComputer.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace OnBoardComputer
+{
+    partial class OnBoardComputer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.pnlInfo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.arbol = new System.Windows.Forms.TreeView();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.pnlInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.ForeColor = System.Drawing.Color.PaleGreen;
+            this.btnCargar.Location = new System.Drawing.Point(12, 12);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(228, 44);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // pnlInfo
+            // 
+            this.pnlInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(60)))), ((int)(((byte)(70)))));
+            this.pnlInfo.Controls.Add(this.label1);
+            this.pnlInfo.Location = new System.Drawing.Point(262, 12);
+            this.pnlInfo.Name = "pnlInfo";
+            this.pnlInfo.Size = new System.Drawing.Size(610, 270);
+            this.pnlInfo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text";
+            // 
+            // arbol
+            // 
+            this.arbol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(60)))), ((int)(((byte)(70)))));
+            this.arbol.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.arbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.arbol.ForeColor = System.Drawing.Color.PaleGreen;
+            this.arbol.Location = new System.Drawing.Point(12, 136);
+            this.arbol.Name = "arbol";
+            this.arbol.Size = new System.Drawing.Size(228, 363);
+            this.arbol.TabIndex = 2;
+            this.arbol.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.arbol_AfterSelect);
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
+            this.cmbCategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCategoria.ForeColor = System.Drawing.Color.PaleGreen;
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(12, 80);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(228, 39);
+            this.cmbCategoria.TabIndex = 3;
+            // 
+            // OnBoardComputer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(884, 511);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.arbol);
+            this.Controls.Add(this.pnlInfo);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "OnBoardComputer";
+            this.Text = "Computer";
+            this.Load += new System.EventHandler(this.OnBoardComputer_Load);
+            this.pnlInfo.ResumeLayout(false);
+            this.pnlInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Panel pnlInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView arbol;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+    }
+}
+
---
+++ OnBoardComputer/OnBoardComputer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace OnBoardComputer
+{
+    public partial class OnBoardComputer : Form
+    {
+        XDocument info;
+
+        public OnBoardComputer()
+        {
+            InitializeComponent();
+        }
+
+        private void OnBoardComputer_Load(object sender, EventArgs e)
+        {
+            info = XDocument.Load(@"DataBank.xml");
+
+            cmbCategoria.Items.Add("");
+            cmbCategoria.Items.Add("Planetas");
+            cmbCategoria.Items.Add("Rutas");
+            cmbCategoria.Items.Add("Regiones");
+            cmbCategoria.Items.Add("Mapas");
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+
+            if (cmbCategoria.SelectedItem != null)
+            {
+                arbol.Nodes.Clear();
+                TreeNode raiz = new TreeNode();
+                raiz.Text = cmbCategoria.SelectedItem.ToString();
+                int index = cmbCategoria.SelectedIndex;
+                string nombreCampo = "";
+
+                if (index == 1)
+                {
+                    nombreCampo = "name";
+                }
+                else if (index == 2)
+                {
+                    nombreCampo = "route";
+                }
+                else if (index == 3 || index == 4)
+                {
+                    nombreCampo = "nameRegion";
+                }
+
+                raiz = agregarNodosARaiz(raiz, nombreCampo);
+                arbol.Nodes.Add(raiz);
+            }
+        }
+
+        TreeNode agregarNodosARaiz(TreeNode raiz, string nombre)
+        {
+            var conjunto = info.Descendants(nombre);
+            foreach (XElement elemento in conjunto)
+            {
+                if (!raiz.Nodes.ContainsKey(elemento.Value))
+                {
+                    raiz.Nodes.Add(elementoANodo(elemento));
+                }
+            }
+            return raiz;
+        }
+
+        TreeNode elementoANodo(XElement elemento)
+        {
+            TreeNode nodo = new TreeNode();
+            nodo.Text = elemento.Value.Replace("-", " ");
+            nodo.Name = elemento.Value.Replace("-", " ");
+            return nodo;
+        }
+
+        private void arbol_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (arbol.SelectedNode.Text != null)
+            {
+                MessageBox.Show(arbol.SelectedNode.Text);
+                //XElement selectedSpaceship = info.Descendants("InfoDetail")
+                //.Where(x => x.Element("idInfoDetail").Value == tree.SelectedNode.Tag.ToString())
+                //.SingleOrDefault();
+
+                //panel.BackColor = Color.FromArgb(255, 50, 60, 70);
+
+                //lblManufacturerTitle.Text = "Manufacturer";
+                //lblLengthTitle.Text = "Length";
+                //lblHyperDriveTitle.Text = "HyperDrive";
+                //lblArmamentTitle.Text = "Armament";
+                //lblSpeedTitle.Text = "Speed";
+                //lblShieldTitle.Text = "Shielding";
+
+                //lblTitle.Text = selectedSpaceship.Element("title").Value;
+                //lblTextInfoDetail.Text = selectedSpaceship.Element("textInfoDetail").Value;
+                //lblManufacturer.Text = selectedSpaceship.Element("Data").Element("Manufacturer").Value;
+                //lblLength.Text = selectedSpaceship.Element("Data").Element("Length").Value;
+                //lblHyperDrive.Text = selectedSpaceship.Element("Data").Element("Hyperdrive").Value;
+                //lblArmament.Text = selectedSpaceship.Element("Data").Element("Armament").Value;
+                //lblSpeed.Text = selectedSpaceship.Element("Data").Element("Speed").Value;
+                //lblShield.Text = selectedSpaceship.Element("Data").Element("Shielding").Value;
+
+                //string imagePath = selectedSpaceship.Element("Blueprint").Value;
+                //pictureBox.Image = Image.FromFile(@"Ships/" + imagePath);
+            }
+        }
+    }
+}
