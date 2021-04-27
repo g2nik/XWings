3abb9c4b0b4d37b4bd320a3990058a5fb466e876
mario5698 <aranicbiagm@gmail.com>
2021-04-27 18:00:32 +0200

    critical system done

---
+++ Critical System/Critical System/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Critical_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pgb_armament = new System.Windows.Forms.ProgressBar();
+            this.pgb_sensor = new System.Windows.Forms.ProgressBar();
+            this.pgb_motor = new System.Windows.Forms.ProgressBar();
+            this.pgb_generador = new System.Windows.Forms.ProgressBar();
+            this.pgb_refector = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Armament";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Motor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Generador ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Reflector";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sensor";
+            // 
+            // pgb_armament
+            // 
+            this.pgb_armament.Location = new System.Drawing.Point(162, 22);
+            this.pgb_armament.Maximum = 60;
+            this.pgb_armament.Name = "pgb_armament";
+            this.pgb_armament.Size = new System.Drawing.Size(432, 23);
+            this.pgb_armament.TabIndex = 5;
+            // 
+            // pgb_sensor
+            // 
+            this.pgb_sensor.Location = new System.Drawing.Point(162, 134);
+            this.pgb_sensor.Maximum = 60;
+            this.pgb_sensor.Name = "pgb_sensor";
+            this.pgb_sensor.Size = new System.Drawing.Size(432, 23);
+            this.pgb_sensor.TabIndex = 6;
+            // 
+            // pgb_motor
+            // 
+            this.pgb_motor.Location = new System.Drawing.Point(162, 47);
+            this.pgb_motor.Maximum = 60;
+            this.pgb_motor.Name = "pgb_motor";
+            this.pgb_motor.Size = new System.Drawing.Size(432, 23);
+            this.pgb_motor.TabIndex = 7;
+            // 
+            // pgb_generador
+            // 
+            this.pgb_generador.Location = new System.Drawing.Point(162, 76);
+            this.pgb_generador.Maximum = 60;
+            this.pgb_generador.Name = "pgb_generador";
+            this.pgb_generador.Size = new System.Drawing.Size(432, 23);
+            this.pgb_generador.TabIndex = 8;
+            // 
+            // pgb_refector
+            // 
+            this.pgb_refector.Location = new System.Drawing.Point(162, 105);
+            this.pgb_refector.Maximum = 60;
+            this.pgb_refector.Name = "pgb_refector";
+            this.pgb_refector.Size = new System.Drawing.Size(432, 23);
+            this.pgb_refector.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(298, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pgb_refector);
+            this.Controls.Add(this.pgb_generador);
+            this.Controls.Add(this.pgb_motor);
+            this.Controls.Add(this.pgb_sensor);
+            this.Controls.Add(this.pgb_armament);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar pgb_armament;
+        private System.Windows.Forms.ProgressBar pgb_sensor;
+        private System.Windows.Forms.ProgressBar pgb_motor;
+        private System.Windows.Forms.ProgressBar pgb_generador;
+        private System.Windows.Forms.ProgressBar pgb_refector;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Critical System/Critical System/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BibliotecaXWigns;
+
+namespace Critical_System
+{
+    public partial class Form1 : Form
+    {
+        string[] llibres = new string[5] { "http://www.gutenberg.org/files/120/120-0.txt", "http://www.gutenberg.org/cache/epub/57303/pg57303.txt", "http://www.gutenberg.org/cache/epub/1228/pg1228.txt", "http://www.gutenberg.org/cache/epub/2000/pg2000.txt", "http://www.gutenberg.org/files/1524/1524-0.txt" };
+        Thread hilo1;
+        ProgressBar[] progressBars;
+        BibliotecaXWigns.HelperMethods hlp = new BibliotecaXWigns.HelperMethods();
+
+        public Form1()
+        {
+        InitializeComponent();
+            progressBars = new ProgressBar[5] { pgb_armament, pgb_motor, pgb_generador, pgb_refector, pgb_sensor };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hilo1 = new Thread(new ThreadStart(startProcess));
+            hilo1.Start();
+        }
+
+        private void startProcess()
+        {
+
+            Parallel.For(0, llibres.Length, (i, state) =>
+            {
+                String direction = "";
+                WebRequest request = WebRequest.Create(llibres[i]);
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                    {
+                        direction = stream.ReadToEnd();
+                        dotChecks(direction, progressBars[i]);
+                    }
+                }
+            });
+        }
+        private void dotChecks(string llibre, ProgressBar pgb)
+        {
+            string[] words = llibre.Split(' ');
+            processBook(words, pgb);
+        }
+
+        private void processBook(string[] words, ProgressBar pgb)
+        {
+            string[] Searchwords = { "white", "time", "that", "the", "empty", "door", "table" };
+            string[] SearchLetter = { "A", "C", "W", "Z", "L", "S", "E" };
+
+            Random rnd = new Random();
+
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine(hlp.GetMostCommonWords(words, rnd.Next(4, 10), rnd.Next(5, 10)));
+                updateProgressbar(pgb);
+            },
+            () =>
+            {
+                
+                Console.WriteLine(hlp.GetMostCommonWordsByLength(words, rnd.Next(4, 10), rnd.Next(5, 10)));
+                updateProgressbar(pgb);
+
+            },
+            () =>
+            {
+                for (int o = 0; o < Searchwords.Length; o++)
+                {
+                    Console.WriteLine(hlp.GetCountForWord(words, Searchwords[o]));
+                    Console.WriteLine(hlp.GetCountForLetter(words, Searchwords[o]));
+                }
+                updateProgressbar(pgb);
+                updateProgressbar(pgb);
+            },
+            () =>
+            {
+                Console.WriteLine(hlp.GetLessCommonWords(words, rnd.Next(4, 10), rnd.Next(5, 10)));
+                updateProgressbar(pgb);
+            }, () =>
+            {
+                Console.WriteLine(hlp.GetLongestWord(words));
+                updateProgressbar(pgb);
+            }
+            );
+        }
+
+
+       
+
+        private void updateProgressbar(ProgressBar pgb)
+        {
+            if (InvokeRequired)
+            {
+                pgb.Invoke(new MethodInvoker(delegate ()
+                {
+                    pgb.Value += 10;
+                }));
+            }
+        }
+    }
+}
