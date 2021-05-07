45b4428b783254d03cab24f4c08dc7995ed5b2cb
g2nik <nikita.chechel@sarria.salesians.cat>
2021-05-07 17:17:19 +0200

    Weapons system niquelado

--- WeaponsSystem/Form1.cs
+++ WeaponsSystem/Form1.cs
@@ -20,11 +20,6 @@ namespace WeaponsSystem
         const int movementY = 15;
         bool fixedTarget = false;
 
-        bool firstLine = false;
-        bool secondLine = false;
-        bool thirdLine = false;
-        bool fourthLine = false;
-
         int beforeVideoCount = 0;
         int afterVideoCount = 0;
 
@@ -36,8 +31,10 @@ namespace WeaponsSystem
         private void Form1_Load(object sender, EventArgs e)
         {
             CheckForIllegalCrossThreadCalls = false;
-            lblTipo.Visible = false;
-            video.Visible = false;
+            lblPlanos.Visible = false;
+            videoDestruccion.Visible = false;
+            videoModelo.Visible = false;
+            videoModelo.settings.setMode("loop", true);
             btnAtacar.Visible = false;
             dibujarNave();
             tDetectar = new Thread(detectar);
@@ -57,10 +54,10 @@ namespace WeaponsSystem
         {
             if (detectarNave(false))
             {
-                beforeVideo.Enabled = true;
+                beforeVideoTimer.Enabled = true;
                 fixedTarget = true;
                 paintColor = Color.Red;
-                image.Image = Image.FromFile("xwing-enemy.png");
+                imagenNave.Image = Image.FromFile("naves/xwing-enemy.png");
                 Invalidate();
                 Update();
                 Refresh();
@@ -69,20 +66,33 @@ namespace WeaponsSystem
 
         private async void detectar()
         {
-            string texto = await fileToString("targets.txt");
-            string[] lineas = texto.Split('\n');
-            Random r = new Random();
-            int aleatorio = r.Next(lineas.Length);
-            bool aliado = lineas[aleatorio].Substring(lineas[aleatorio].Length - 2, 1) == "A";
-            lblTipo.Visible = true;
-            lblTipo.Text = aliado ? "Aliado" : "Enemigo";
-            lblTipo.ForeColor = aliado ? Color.PaleGreen : Color.Red;
-            btnAtacar.Visible = !aliado;
-            paintColor = aliado ? Color.PaleGreen : Color.Red;
-            image.Image = aliado ? Image.FromFile("xwing-ally.png") : Image.FromFile("xwing-enemy.png");
-            Invalidate();
-            Update();
-            Refresh();
+            if (detectarNave())
+            {
+                string texto = File.Exists("targets.txt") ? File.ReadAllText("targets.txt") : "";
+                string[] lineas = texto.Split('\n');
+
+                Random r = new Random();
+                int aleatorio = r.Next(lineas.Length);
+                string nombreNave = lineas[aleatorio].Substring(0, lineas[aleatorio].IndexOf('|'));
+                bool aliado = lineas[aleatorio].Substring(lineas[aleatorio].Length - 2, 1) == "A";
+
+                lblPlanos.Visible = true;
+                lblPlanos.Text = aliado ? $"Planos de {nombreNave} aliado" : $"Planos de {nombreNave} enemigo";
+
+                videoModelo.Visible = true;
+                videoModelo.URL = $"videos/{nombreNave}.mp4";
+                videoModelo.Ctlcontrols.play();
+                imagenPlanos.Visible = true;
+                imagenPlanos.Image = Image.FromFile($"planos/{nombreNave}.jpg");
+
+                lblPlanos.ForeColor = aliado ? Color.PaleGreen : Color.Red;
+                btnAtacar.Visible = !aliado;
+                paintColor = aliado ? Color.PaleGreen : Color.Red;
+                imagenNave.Image = aliado ? Image.FromFile("naves/xwing-ally.png") : Image.FromFile("naves/xwing-enemy.png");
+                Invalidate();
+                Update();
+                Refresh();
+            }
             tDetectar.Abort();
         }
 
@@ -90,17 +100,17 @@ namespace WeaponsSystem
         {
             bool result = false;
 
-            if (image.Location.X >= 230 && image.Location.X <= 260
-                && image.Location.Y >= 180 && image.Location.Y <= 210)
+            if (imagenNave.Location.X >= 230 && imagenNave.Location.X <= 260
+                && imagenNave.Location.Y >= 180 && imagenNave.Location.Y <= 210)
             {
                 result = true;
                 paintColor = Color.Yellow;
-                image.Image = Image.FromFile("xwing-detected.png");
+                imagenNave.Image = Image.FromFile("naves/xwing-detected.png");
             }
             else
             {
                 paintColor = Color.Cyan;
-                image.Image = Image.FromFile("xwing.png");
+                imagenNave.Image = Image.FromFile("naves/xwing.png");
             }
 
             if (refresh)
@@ -113,17 +123,36 @@ namespace WeaponsSystem
             return result;
         }
 
-        private async Task<string> fileToString(string path)
-        {
-            return File.Exists(path) ? File.ReadAllText(path) : "";
-        }
-
         private void dibujarNave()
         {
             Random r = new Random();
             int x = r.Next(50, 450);
             int y = r.Next(50, 350);
-            image.Location = new Point(x, y);
+            imagenNave.Location = new Point(x, y);
+        }
+
+        private void btnArriba_Click(object sender, EventArgs e)
+        {
+            imagenNave.Location = new Point(imagenNave.Location.X, imagenNave.Location.Y - movementY);
+            detectarNave();
+        }
+
+        private void btnAbajo_Click(object sender, EventArgs e)
+        {
+            imagenNave.Location = new Point(imagenNave.Location.X, imagenNave.Location.Y + movementY);
+            detectarNave();
+        }
+
+        private void btnDerecha_Click(object sender, EventArgs e)
+        {
+            imagenNave.Location = new Point(imagenNave.Location.X + movementX, imagenNave.Location.Y);
+            detectarNave();
+        }
+
+        private void btnIzquierda_Click(object sender, EventArgs e)
+        {
+            imagenNave.Location = new Point(imagenNave.Location.X - movementX, imagenNave.Location.Y);
+            detectarNave();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -137,140 +166,93 @@ namespace WeaponsSystem
                 new Rectangle(155, 155, 300, 200),
                 new Rectangle(205, 205, 200, 100),
             };
-            
+
             e.Graphics.DrawRectangles(pen, rectArray);
 
             if (fixedTarget)
             {
                 int initialX1 = 5;
-                int initialX2 = 605;
                 int initialY1 = 5;
+
+                int initialX2 = 605;
                 int initialY2 = 505;
 
-                if (firstLine)
+                if (beforeVideoCount == 0)
                 {
                     e.Graphics.DrawLine(pen, initialX1, initialY1, 55, 55);
                     e.Graphics.DrawLine(pen, initialX2, initialY1, 555, 55);
                     e.Graphics.DrawLine(pen, initialX1, initialY2, 55, 455);
                     e.Graphics.DrawLine(pen, initialX2, initialY2, 555, 455);
                 }
-                if (secondLine)
+                else if (beforeVideoCount == 1)
                 {
                     e.Graphics.DrawLine(pen, initialX1, initialY1, 105, 105);
                     e.Graphics.DrawLine(pen, initialX2, initialY1, 505, 105);
                     e.Graphics.DrawLine(pen, initialX1, initialY2, 105, 405);
                     e.Graphics.DrawLine(pen, initialX2, initialY2, 505, 405);
                 }
-                if (thirdLine)
+                else if(beforeVideoCount == 2)
                 {
                     e.Graphics.DrawLine(pen, initialX1, initialY1, 155, 155);
                     e.Graphics.DrawLine(pen, initialX2, initialY1, 455, 155);
                     e.Graphics.DrawLine(pen, initialX1, initialY2, 155, 355);
                     e.Graphics.DrawLine(pen, initialX2, initialY2, 455, 355);
                 }
-                if (fourthLine)
+                else //(beforeVideoCount == 3)
                 {
                     e.Graphics.DrawLine(pen, initialX1, initialY1, 205, 205);
                     e.Graphics.DrawLine(pen, initialX2, initialY1, 405, 205);
                     e.Graphics.DrawLine(pen, initialX1, initialY2, 205, 305);
                     e.Graphics.DrawLine(pen, initialX2, initialY2, 405, 305);
                 }
-
-                //if (firstLine) e.Graphics.DrawLine(pen, 5, 5, 205, 205);
-                //if (secondLine)  e.Graphics.DrawLine(pen, 605, 5, 405, 205);
-                //if (thirdLine) e.Graphics.DrawLine(pen, 5, 505, 205, 305);
-                //if (fourthLine) e.Graphics.DrawLine(pen, 605, 505, 405, 305);
             }
             pen.Dispose();
         }
 
-        private void btnArriba_Click(object sender, EventArgs e)
-        {
-            image.Location = new Point(image.Location.X, image.Location.Y - movementY);
-            detectarNave();
-        }
-
-        private void btnAbajo_Click(object sender, EventArgs e)
-        {
-            image.Location = new Point(image.Location.X, image.Location.Y + movementY);
-            detectarNave();
-        }
-
-        private void btnDerecha_Click(object sender, EventArgs e)
-        {
-            image.Location = new Point(image.Location.X + movementX, image.Location.Y);
-            detectarNave();
-        }
-
-        private void btnIzquierda_Click(object sender, EventArgs e)
-        {
-            image.Location = new Point(image.Location.X - movementX, image.Location.Y);
-            detectarNave();
-        }
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Up)
-            {
-                image.Location = new Point(image.Location.X, image.Location.Y - movementY);
-                detectarNave();
-            }
-        }
-
         private void beforeVideo_Tick(object sender, EventArgs e)
         {
-            if (beforeVideoCount == 1)
+            if (beforeVideoCount == 4)
             {
-                firstLine = true;
+                beforeVideoTimer.Enabled = false;
+                beforeVideoCount = 0;
+                afterVideoTimer.Enabled = true;
+            } else {
+                beforeVideoCount++;
+                Invalidate();
+                Update();
+                Refresh();
             }
-            else if (beforeVideoCount == 2)
-            {
-                secondLine = true;
-            }
-            else if (beforeVideoCount == 3)
-            {
-                thirdLine = true;
-            }
-            else if (beforeVideoCount == 4)
-            {
-                fourthLine = true;
-            }
-            else if (beforeVideoCount == 5)
-            {
-                beforeVideo.Enabled = false;
-                afterVideo.Enabled = true;
-
-            }
-            beforeVideoCount++;
-            Invalidate();
-            Update();
-            Refresh();
         }
 
         private void afterVideo_Tick(object sender, EventArgs e)
         {
-            if (afterVideoCount == 1)
+            if (afterVideoCount == 0)
             {
-                video.Visible = true;
-                video.Ctlcontrols.play();
+                videoDestruccion.Visible = true;
+                videoDestruccion.Ctlcontrols.play();
+                afterVideoCount++;
             }
-            else if (afterVideoCount == 3)
+            else if (afterVideoCount == 2)
             {
-                afterVideo.Enabled = false;
-                video.Visible = false;
-                fixedTarget = false;
-                lblTipo.Visible = false;
-                btnAtacar.Visible = false;
-                beforeVideoCount = 0;
+                videoDestruccion.Ctlcontrols.stop();
+                afterVideoTimer.Enabled = false;
                 afterVideoCount = 0;
-                firstLine = false;
-                secondLine = false;
-                thirdLine = false;
-                fourthLine = false;
+
+                fixedTarget = false;
+                videoDestruccion.Visible = false;
+                videoModelo.Visible = false;
+                imagenPlanos.Visible = false;
+                lblPlanos.Visible = false;
+                btnAtacar.Visible = false;
+                
+                paintColor = Color.Cyan;
+                imagenNave.Image = Image.FromFile("naves/xwing.png");
+                dibujarNave();
                 Invalidate();
+                Update();
+                Refresh();
             }
-            
-            afterVideoCount++;
+            else afterVideoCount++;
         }
     }
 }
