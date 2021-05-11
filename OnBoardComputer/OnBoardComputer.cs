1c0906e5459773af5e9d731d42c9e08dfba35ff3
mario5698 <arancibiagm@gmail.com>
2021-05-11 17:13:48 +0200

    Refactoring OnBoardComputer Done

--- OnBoardComputer/OnBoardComputer.cs
+++ OnBoardComputer/OnBoardComputer.cs
@@ -16,7 +16,7 @@ namespace OnBoardComputer
     {
         XDocument info;
         List<string> categorias = new List<string>();
-
+        String nombreElemento;
         public OnBoardComputer()
         {
             InitializeComponent();
@@ -111,98 +111,124 @@ namespace OnBoardComputer
             {
                 if (!categorias.Contains(arbol.SelectedNode.Text))
                 {
-                    string nombreElemento = arbol.SelectedNode.Text;
+                    nombreElemento = arbol.SelectedNode.Text;
                     int index = cmbCategoria.SelectedIndex;
-
-                    if (index == 1)
-                    {
-                        pictureBox.Visible = true;
-                        pnlSlider.Visible = false;
-                        mostrarEtiquetas(true, 1, 5);
-                        var datosPlaneta = info.Descendants("planet")
-                            .Where(c => c.Descendants("name")
-                            .SingleOrDefault().Value == nombreElemento)
-                            .Descendants();
-
-                        lblTitle1.Text = "Nombre";
-                        lblValue1.Text = datosPlaneta.ElementAt(0).Value;
-                        lblTitle2.Text = "Sector";
-                        lblValue2.Text = datosPlaneta.ElementAt(1).Value;
-                        lblTitle3.Text = "Afiliacion";
-                        lblValue3.Text = datosPlaneta.ElementAt(2).Value;
-                        lblTitle3.Text = "Latitud / longitud / parsecs";
-                        string lat = datosPlaneta.ElementAt(3).Element("lat").Value;
-                        string lng = datosPlaneta.ElementAt(3).Element("long").Value;
-                        string parsecs = lng = datosPlaneta.ElementAt(3).Element("parsecs").Value;
-                        lblValue3.Text = lat + " / " + lng + " / " + parsecs;
-
-                        lblTitle4.Text = "Rutas";
-                        lblValue4.Text = "";
-                        var rutas = datosPlaneta.Descendants("route");
-                        foreach (XElement ruta in rutas) lblValue4.Text += ruta.Value + "\n";
-
-                        lblTitle5.Text = "Nativos";
-                        lblValue5.Text = datosPlaneta.ElementAt(datosPlaneta.Count() - 2).Value;
-                        string formatoImagen = 
-                        File.Exists(@"planets/" + nombreElemento + ".png") ? ".png" : ".jpg";
-                        pictureBox.Image = Image.FromFile(@"planets/" + nombreElemento + formatoImagen);
-                    }
-
-                    else if (index == 2)
-                    {
-                        pictureBox.Visible = true;
-                        pnlSlider.Visible = false;
-                        mostrarEtiquetas(false, 1, 5);
-                        mostrarEtiquetas(true, 1, 3);
-                        var datosRuta = info.Descendants("Route")
-                            .Where(c => c.Descendants("nameRoute")
-                            .SingleOrDefault().Value == nombreElemento)
-                            .Descendants();
-
-                        lblTitle1.Text = "Nombre";
-                        lblValue1.Text = datosRuta.ElementAt(1).Value;
-                        lblTitle2.Text = "Tipo";
-                        lblValue2.Text = datosRuta.ElementAt(0).Value;
-                        lblTitle3.Text = "Inicio";
-                        lblValue3.Text = datosRuta.ElementAt(2).Value;
-                        lblTitle4.Text = "Fin";
-                        lblValue4.Text = datosRuta.ElementAt(3).Value;
-
-                        string[] palabrasElemento = nombreElemento.Split(' ');
-                        bool imagenExiste = Directory.EnumerateFiles(@"planets/").Any(f => f.IndexOf(palabrasElemento[0], StringComparison.OrdinalIgnoreCase) > 0);
-
-                        if (imagenExiste)
-                        {
-                            string[] files = Directory.GetFiles(@"planets/", $"{palabrasElemento[0]}*.*");
-                            pictureBox.Image = Image.FromFile(files[0]);
-                        }
-                        else
-                        {
-                            pictureBox.Image = Image.FromFile("planets/na.jpg");
-                        }
-                    }
-
-                    else if (index == 3)
-                    {
-                        pictureBox.Visible = true;
-                        pnlSlider.Visible = false;
-                        mostrarEtiquetas(false, 1, 5);
-                        mostrarEtiquetas(true, 1, 2);
-                        var datosRegion = info.Descendants("Region")
-                            .Where(c => c.Descendants("nameRegion")
-                            .SingleOrDefault().Value.Replace("-", " ") == nombreElemento)
-                            .Descendants();
-
-                        lblTitle1.Text = "Nombre";
-                        lblValue1.Text = datosRegion.ElementAt(0).Value;
-                        lblTitle2.Text = "Descripcion";
-                        lblValue2.Text = datosRegion.ElementAt(1).Value;
-
-                        pictureBox.Image = Image.FromFile(@"planets/" + datosRegion.ElementAt(2).Value);
-                    }
+                    getdata(index);
                 }
             }
         }
+
+        private void getdata(int index)
+        {
+            if (index ==1 )
+            {
+                getPlanets();
+            }
+            else if(index ==2 )
+            {
+                getRoutes();
+            }
+            else if (index == 3)
+            {
+                getRegions();
+            }
+        }
+        void getPlanets()
+        {
+            pictureBox.Visible = true;
+            pnlSlider.Visible = false;
+            mostrarEtiquetas(true, 1, 5);
+            var datosPlaneta = info.Descendants("planet")
+                .Where(c => c.Descendants("name")
+                .SingleOrDefault().Value == nombreElemento)
+                .Descendants();
+
+            lblTitle1.Text = "Nombre";
+            lblValue1.Text = datosPlaneta.ElementAt(0).Value;
+
+            lblTitle2.Text = "Sector";
+            lblValue2.Text = datosPlaneta.ElementAt(1).Value;
+
+            lblTitle3.Text = "Afiliacion";
+            lblValue3.Text = datosPlaneta.ElementAt(2).Value;
+
+            lblTitle3.Text = "Latitud / longitud / parsecs";
+            string lat = datosPlaneta.ElementAt(3).Element("lat").Value;
+            string lng = datosPlaneta.ElementAt(3).Element("long").Value;
+            string parsecs = lng = datosPlaneta.ElementAt(3).Element("parsecs").Value;
+            lblValue3.Text = lat + " / " + lng + " / " + parsecs;
+
+            lblTitle4.Text = "Rutas";
+            lblValue4.Text = "";
+            var rutas = datosPlaneta.Descendants("route");
+
+            foreach (XElement ruta in rutas) lblValue4.Text += ruta.Value + "\n";
+
+            lblTitle5.Text = "Nativos";
+            lblValue5.Text = datosPlaneta.ElementAt(datosPlaneta.Count() - 2).Value;
+
+            string formatoImagen =
+            File.Exists(@"planets/" + nombreElemento + ".png") ? ".png" : ".jpg";
+            pictureBox.Image = Image.FromFile(@"planets/" + nombreElemento + formatoImagen);
+        }
+        
+        void getRoutes()
+        {
+            pictureBox.Visible = true;
+            pnlSlider.Visible = false;
+            mostrarEtiquetas(false, 1, 5);
+            mostrarEtiquetas(true, 1, 3);
+            var datosRuta = info.Descendants("Route")
+                .Where(c => c.Descendants("nameRoute")
+                .SingleOrDefault().Value == nombreElemento)
+                .Descendants();
+
+            lblTitle1.Text = "Nombre";
+            lblValue1.Text = datosRuta.ElementAt(1).Value;
+
+            lblTitle2.Text = "Tipo";
+            lblValue2.Text = datosRuta.ElementAt(0).Value;
+
+            lblTitle3.Text = "Inicio";
+            lblValue3.Text = datosRuta.ElementAt(2).Value;
+
+            lblTitle4.Text = "Fin";
+            lblValue4.Text = datosRuta.ElementAt(3).Value;
+
+            string[] palabrasElemento = nombreElemento.Split(' ');
+            bool imagenExiste = Directory.EnumerateFiles(@"planets/").Any(f => f.IndexOf(palabrasElemento[0], StringComparison.OrdinalIgnoreCase) > 0);
+
+            if (imagenExiste)
+            {
+                string[] files = Directory.GetFiles(@"planets/", $"{palabrasElemento[0]}*.*");
+                pictureBox.Image = Image.FromFile(files[0]);
+            }
+            else
+            {
+                pictureBox.Image = Image.FromFile("planets/na.jpg");
+            }
+        }
+
+        void getRegions()
+        {
+            pictureBox.Visible = true;
+            pnlSlider.Visible = false;
+            mostrarEtiquetas(false, 1, 5);
+            mostrarEtiquetas(true, 1, 2);
+            var datosRegion = info.Descendants("Region")
+                .Where(c => c.Descendants("nameRegion")
+                .SingleOrDefault().Value.Replace("-", " ") == nombreElemento)
+                .Descendants();
+
+            lblTitle1.Text = "Nombre";
+            lblValue1.Text = datosRegion.ElementAt(0).Value;
+            lblTitle2.Text = "Descripcion";
+            lblValue2.Text = datosRegion.ElementAt(1).Value;
+
+            pictureBox.Image = Image.FromFile(@"planets/" + datosRegion.ElementAt(2).Value);
+        }
+
+      
 
         private void map1_Click(object sender, EventArgs e) => mapBig.Image = map1.Image;
 
